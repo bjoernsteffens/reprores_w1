c169352f5143771b5c5fbf2e90644707c0181ebf
Bjoern W. Steffens <bjoern.steffens1@gmail.com>
2016-02-03 09:58:54 +0100

    Adding the image file for Knitr

--- week1.R
+++ week1.R
@@ -1,3 +1,43 @@
 
 
 setwd("~/mycloud/Private_DataScience/Coursera/10 Data Science Specialisation/40 Reproducible Research/Assignments/week1")
+
+
+#
+# Load the libraries we are going to use
+# and clean out any left-overs in the $env
+rm(list=ls())
+library(ggplot2)
+
+#
+# Read the csv file and transform the date string to a proper date
+steps <- read.csv("activity.csv")
+steps$date <- as.Date(steps$date,"%Y-%m-%d")
+
+#
+# Get the number of steps per day and clean up column names
+stepsDay <- as.data.frame(tapply(steps$steps,steps$date,sum))
+stepsDay$Day <- row.names(stepsDay)
+colnames(stepsDay) <- c("Steps","Day")
+
+#
+# Plot it on the X axis, dont plot legend and turn the x-labels
+png(filename = "reprores1.png", width = 960, height = 640)
+g <- ggplot(stepsDay, aes(x=Day, y=Steps, fill = 20))
+g + geom_bar(stat = "Identity", alpha = 0.9) +
+    theme(axis.text.x = element_text(angle = 90, hjust = 0)) +
+    theme(panel.background = element_rect(fill = "lightblue")) +
+    theme(strip.background = element_rect(fill = "lightblue")) +
+    theme(panel.grid.minor = element_blank()) +
+    theme(panel.grid.major = element_line(colour = "grey95")) +
+    theme(plot.margin=unit(c(2,1,1.5,1.2),"cm")) +
+    scale_y_continuous(labels = scales::comma) +
+    theme(legend.position="none") +
+    xlab("Steps per Day
+         Missing values in \"empty\" days") +
+    theme(axis.text.x = element_text(size=10,margin = margin(0,0,20,0))) +
+    ylab("Number of Recorded Step") + 
+    theme(axis.text.y = element_text(size=10,margin = margin(0,0,0,10))) +
+    ggtitle("Number of Steps Recorded per Day") +
+    theme(plot.title = element_text(size = 20,margin = margin(0,0,30,0)))
+dev.off()   
